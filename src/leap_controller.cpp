e5eb4b26581f41bb72234fcb390772621dba214d
Kdfrizale <kyle.frizzell.14@cnu.edu>
2018-01-29 17:48:16 -0500

    Changed to ROS_INFO, Outlined Smaller functions

--- src/leap_controller.cpp
+++ src/leap_controller.cpp
@@ -15,25 +15,34 @@
 #include <boost/shared_ptr.hpp>
 #include "../include/tf/transform_datatypes.h"
 
-using namespace Leap;
-
-class LeapController : public Listener {
+class LeapController : public Leap::Listener {
 public:
-    virtual void onInit(const Controller&);
-    virtual void onConnect(const Controller&);
-    virtual void onDisconnect(const Controller&);
-    virtual void onExit(const Controller&);
-    virtual void onFrame(const Controller&);
-    virtual void onFocusGained(const Controller&);
-    virtual void onFocusLost(const Controller&);
-    virtual void onDeviceChange(const Controller&);
-    virtual void onServiceConnect(const Controller&);
-    virtual void onServiceDisconnect(const Controller&);
+    LeapController(ros::NodeHandle &nh);
+    virtual void onInit(const Leap::Controller&);
+    virtual void onConnect(const Leap::Controller&);
+    virtual void onDisconnect(const Leap::Controller&);
+    virtual void onExit(const Leap::Controller&);
+    virtual void onFrame(const Leap::Controller&);
+    virtual void onFocusGained(const Leap::Controller&);
+    virtual void onFocusLost(const Leap::Controller&);
+    virtual void onDeviceChange(const Leap::Controller&);
+    virtual void onServiceConnect(const Leap::Controller&);
+    virtual void onServiceDisconnect(const Leap::Controller&);
 private:
+    ros::NodeHandle nh_;
     ros::Publisher hand_pose_publisher_;
+    std::string hand_to_sense_;
 
-    void getFrame();
+    geometry_msgs::PoseStamped sensedPosePalm_;//Center of the Palm Pose
+    std::vector<geometry_msgs::PoseStamped> sensedPoseFingerTips_;//Finger Tip Poses
+    std::vector<geometry_msgs::PoseStamped> sensedPoseMisc_;//Anything Extra
+
+    Leap::Frame current_frame_;
+
     void processFrame();
+    void processHand(const Leap::Hand& aHand);
+    void processFinger(const Leap::Finger& aFinger);
+    void translateToRosCoordinate();
     void publishHandPose();
 
 };
@@ -41,232 +50,107 @@ private:
 const std::string fingerNames[] = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
 const std::string boneNames[] = {"Metacarpal", "Proximal", "Middle", "Distal"};
 const std::string stateNames[] = {"STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END"};
-//ros::Publisher publish;
-//const long outOfBoundsState = 123456789.0;
 
-void LeapController::onInit(const Controller& controller) {
-    std::cout << "Initialized" << std::endl;
+LeapController::LeapController(ros::NodeHandle &nh):nh_(nh){
+  std::string output_pose_topic_name;
+  nh_.getParam("leap_output_pose_topic",output_pose_topic_name);
+  ROS_INFO("Publishing Leap Hand Poses to topic [%s]", output_pose_topic_name.c_str());
+  hand_pose_publisher_ = nh_.advertise<leap_controller_capstone::HandPoseStamped>(output_pose_topic_name,10);
+
+  nh_.param<std::string>("hand_to_sense",hand_to_sense_, "first");
 }
 
-void LeapController::onConnect(const Controller& controller) {
-    std::cout << "Connected" << std::endl;
+void LeapController::onFrame(const Leap::Controller& controller){
+  current_frame_ = controller.frame();
+  processFrame();
+
 }
 
-void LeapController::onDisconnect(const Controller& controller) {
+void LeapController::processFrame(){
+  enum HandToSense {first, second, left, right, both};
+  //TODO determine which hand should be processed based on ROS param
+  //Then process that hand
+  Leap::HandList hands = current_frame_.hands();
+  for (Leap::HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
+      const Leap::Hand hand = *hl;
+      processHand(hand);
+    }
+    publishHandPose();
+}
+
+void LeapController::processHand(const Leap::Hand& aHand){
+  const Leap::Vector normal = aHand.palmNormal();
+  const Leap::Vector direction = aHand.direction();
+  //TODO add logic code to get important information on the palm here
+
+  const Leap::FingerList fingers = aHand.fingers();
+  for (Leap::FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
+      const Leap::Finger finger = *fl;
+      processFinger(finger);
+    }
+
+}
+
+void LeapController::processFinger(const Leap::Finger& aFinger){
+  //TODO add logic code to get all the information for the finger
+}
+
+void LeapController::publishHandPose(){
+  leap_controller_capstone::HandPoseStamped msg;
+  msg.posePalm = sensedPosePalm_;
+  msg.poseFingerTips = sensedPoseFingerTips_;
+  msg.poseMisc = sensedPoseMisc_;
+  hand_pose_publisher_.publish(msg);
+}
+
+void LeapController::onInit(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Initialized");
+}
+
+void LeapController::onConnect(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Connected");
+}
+
+void LeapController::onDisconnect(const Leap::Controller& controller) {
     // Note: not dispatched when running in a debugger.
-    std::cout << "Disconnected" << std::endl;
+    ROS_INFO("Leap Controller: Disconnected");
 }
 
-void LeapController::onExit(const Controller& controller) {
-    std::cout << "Exited" << std::endl;
+void LeapController::onExit(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Exited");
 }
 
-void LeapController::onFrame(const Controller& controller) {
-    // Get the most recent frame and report some basic information
-    const Frame frame = controller.frame();
-    std::cout << "Frame id: " << frame.id() << std::endl;
-
-    //All hands recorded on the frame
-    HandList hands = frame.hands();
-
-    //Struct of the 3 points that I need to send via ROS MSG
-    geometry_msgs::PoseStamped sensedPoseTip1;//Thumb Finger tip
-    geometry_msgs::PoseStamped sensedPoseTip2;//Index Finger tip
-    geometry_msgs::PoseStamped sensedPosePalm;//wrist
-
-//************************************************ Trial: remove for loop and see if it still runs as normal *************************************************************************
-    for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
-        // Get the first hand
-        const Hand hand = *hl;
-
-        //FIRST HAND ONLY
-        if(hand == frame.hands() [0]){
-            std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
-            std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
-//maybe delete info below/ see the number below and maybe use this value instead of Center of Palm
-            << ", palm position: " << hand.palmPosition() << std::endl;
-
-            // Get the hand's normal vector and direction
-            const Vector normal = hand.palmNormal();
-            const Vector direction = hand.direction();
-
-            // Calculate the hand's pitch, roll, and yaw angles
-            std::cout << std::string(2, ' ') <<  "pitch: " << direction.pitch() << " Rads, "
-            << "roll: " << normal.roll() << " Rads, "
-            << "yaw: " << direction.yaw() << " Rads" << std::endl;
-
-            // Get fingers
-            const FingerList fingers = hand.fingers();
-            for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
-                const Finger finger = *fl;
-
-                //Added the if statement to only print Thumb and Index finger.
-                if(fingerNames[finger.type()] == fingerNames[0] || fingerNames[finger.type()] == fingerNames[1]
-                   || fingerNames[finger.type()] == fingerNames[2]){
-                    std::cout << std::string(4, ' ') <<  fingerNames[finger.type()]
-//maybe delete info below.
-                    << " finger, id: " << finger.id()
-                    << ", length: " << finger.length()
-                    << "mm, width: " << finger.width() << std::endl;
-                }
-
-                //The Start and End points of each bone is the frame coordinates of where the bones begin and end
-                // Get finger bones
-                for (int b = 0; b < 4; ++b) {
-                    Bone::Type boneType = static_cast<Bone::Type>(b);
-                    Bone bone = finger.bone(boneType);
-
-                    //Center of Palm = (Middle Metacarpal Start + End)/2 (x,y,z) (Yaw, Roll, Pitch)
-                    if(fingerNames[finger.type()] == fingerNames[3] && boneNames[boneType] == boneNames[0]){
-                            //Center of Palm
-                            std::cout << std::string(6, ' ') <<  "Center of Palm " << bone.center() << std::endl;
-
-                            //Copied from Kyle Frizzell's myDummyPub publisher
-                            //Values in meters (mm/1000).
-                            sensedPosePalm.header.frame_id = "m1n6s200_link_base";
-                            sensedPosePalm.pose.position.x = -(bone.center().x/1000);
-                            sensedPosePalm.pose.position.y = (bone.center().z/1000);
-                            sensedPosePalm.pose.position.z = (bone.center().y/1000);
-
-                            //Conversion Trial1: For ARM settings
-                            // double yaw = (bone.prevJoint().roll());
-                            // double roll = (bone.prevJoint().pitch());
-                            // double pitch = (bone.prevJoint().yaw());
-
-                            //Finger Based Values
-                            // double yaw = (bone.prevJoint().yaw());
-                            // double roll = (bone.prevJoint().roll());
-                            // double pitch = (bone.prevJoint().pitch());
-
-                            //Hand based values
-                            double yaw = -direction.yaw();
-                            // double roll = normal.roll();
-                            double roll = normal.roll();
-                            double pitch = direction.pitch();
-                            std::cout << "Normal   (R,P,Y): (" << normal.roll() << ", " << normal.pitch() << ", " << normal.yaw() << ")" << std::endl;
-                            std::cout << "Direction (R,P,Y): (" << direction.roll() << ", " << direction.pitch() << ", " << direction.yaw() << ")" << std::endl;
-                            //Hand based values Conversion Trials 1
-                            // double pitch = direction.yaw();
-                            // double yaw = normal.roll();
-                            // double roll = direction.pitch();
-
-                            //Hand based values Conversion Trials 2
-                            // double roll = direction.yaw();
-                            // double yaw = normal.roll();
-                            // double pitch = direction.pitch();
-
-                            //Vector Converter for ROS
-                            //Changes yaw, pitch, roll into
-                            //(yaw pitch roll angle);(x y z w)
-                            tf::Matrix3x3 space;
-                            space.setEulerYPR(yaw,pitch,roll);
-                            tf::Quaternion quater;
-                            space.getRotation(quater);
-
-                            tf::Quaternion quater2 = tf::createQuaternionFromRPY(roll, pitch, yaw);
-
-
-
-                            //Puts converted data into the ROS struct
-                            std::cout << "Quater Values: " << std::endl
-                            << "X: " << quater.getX() << "  Y: " << quater.getY()
-                            << "  Z: " << quater.getZ() << "  W: " << quater.getW() << std::endl;
-
-                            std::cout << "Second Quater Values: " << std::endl
-                            << "X: " << quater2.getX() << "  Y: " << quater2.getY()
-                            << "  Z: " << quater2.getZ() << "  W: " << quater2.getW() << std::endl;
-                            sensedPosePalm.pose.orientation.x = quater.getX();
-                            sensedPosePalm.pose.orientation.y = quater.getY();
-                            sensedPosePalm.pose.orientation.z = quater.getZ();
-                            sensedPosePalm.pose.orientation.w = quater.getW();
-                    }
-
-                    //Finger Tip Thumb = Thumb Distal Start (x,y,z)
-                    if(fingerNames[finger.type()] == fingerNames[0] && boneNames[boneType] == boneNames[3]){
-                            std::cout << std::string(6, ' ') << "Finger Tip: " << bone.prevJoint() << std::endl;
-                            sensedPoseTip1.header.frame_id = "m1n6s200_link_base";
-                            sensedPoseTip1.pose.position.x = -(double)((bone.prevJoint().x)/1000);
-                            sensedPoseTip1.pose.position.y = (double)((bone.prevJoint().z)/1000);
-                            sensedPoseTip1.pose.position.z = (double)((bone.prevJoint().y)/1000);
-                    }
-
-                    //Finger Tip Index = Index Distal Start (x,y,z)
-                    //Changed to Index proximal (start + end)/2 (x,y,z)
-                    if(fingerNames[finger.type()] == fingerNames[1] && boneNames[boneType] == boneNames[1]){
-                            std::cout << std::string(6, ' ') << "Index \"Finger Tip\": " << bone.prevJoint() << std::endl;
-                            sensedPoseTip2.header.frame_id = "m1n6s200_link_base";
-                            sensedPoseTip2.pose.position.x = -(double)((bone.prevJoint().x)/1000);
-                            sensedPoseTip2.pose.position.y = (double)((bone.prevJoint().z)/1000);
-                            sensedPoseTip2.pose.position.z = (double)((bone.prevJoint().y)/1000);
-                            // sensedPoseTip2.pose.position.x = -(double)((bone.prevJoint().x)/1000);
-                            // sensedPoseTip2.pose.position.y = (double)((bone.prevJoint().z)/1000);
-                            // sensedPoseTip2.pose.position.z = (double)((bone.prevJoint().y)/1000);
-                    }
-                }
-            }
-        }
-    }
-    if(hands.isEmpty()){
-      // std::cout << "Error State: No Hands" << std::endl;
-      sensedPoseTip1.header.frame_id = "m1n6s200_link_base";
-      sensedPoseTip1.pose.position.x = outOfBoundsState;
-      sensedPoseTip1.pose.position.y = outOfBoundsState;
-      sensedPoseTip1.pose.position.z = outOfBoundsState;
-
-      sensedPoseTip2.header.frame_id = "m1n6s200_link_base";
-      sensedPoseTip2.pose.position.x = outOfBoundsState;
-      sensedPoseTip2.pose.position.y = outOfBoundsState;
-      sensedPoseTip2.pose.position.z = outOfBoundsState;
-
-      sensedPosePalm.header.frame_id = "m1n6s200_link_base";
-      sensedPosePalm.pose.position.x = outOfBoundsState;
-      sensedPosePalm.pose.position.y = outOfBoundsState;
-      sensedPosePalm.pose.position.z = outOfBoundsState;
-
-    }
-    //Puts everything in one nice package and sends the data
-    leap_controller_capstone::HandPoseStamped thread;
-    //thread.poseTip2 = sensedPoseTip2;
-    //thread.poseTip1 = sensedPoseTip1;
-    thread.posePalm = sensedPosePalm;
-    publish.publish(thread);
+void LeapController::onFocusGained(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Focus Gained");
 }
 
-void LeapController::onFocusGained(const Controller& controller) {
-    std::cout << "Focus Gained" << std::endl;
+void LeapController::onFocusLost(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Focus Lost");
 }
 
-void LeapController::onFocusLost(const Controller& controller) {
-    std::cout << "Focus Lost" << std::endl;
-}
-
-void LeapController::onDeviceChange(const Controller& controller) {
-    std::cout << "Device Changed" << std::endl;
-    const DeviceList devices = controller.devices();
+void LeapController::onDeviceChange(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Device Changed");
+    const Leap::DeviceList devices = controller.devices();
 
     for (int i = 0; i < devices.count(); ++i) {
-        std::cout << "id: " << devices[i].toString() << std::endl;
-        std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+        ROS_INFO("Leap Controller: id: [%s]", devices[i].toString().c_str());
+        ROS_INFO("  isStreaming:  [%s]",(devices[i].isStreaming() ? "true" : "false"));
     }
 }
 
-void LeapController::onServiceConnect(const Controller& controller) {
-    std::cout << "Service Connected" << std::endl;
+void LeapController::onServiceConnect(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Service Connected");
+
 }
 
-void LeapController::onServiceDisconnect(const Controller& controller) {
-    std::cout << "Service Disconnected" << std::endl;
+void LeapController::onServiceDisconnect(const Leap::Controller& controller) {
+    ROS_INFO("Leap Controller: Service Disconnected");
 }
 
 int main(int argc, char** argv) {
-
     //Defines where the data is to be sent to.
     ros::init(argc, argv, "leap_controller_node");
     ros::NodeHandle node_handle;
-    std::string output_pose_topic_name;
-    node_handle.getParam("leap_output_pose_topic",output_pose_topic_name);
-    ROS_INFO("The output topic is [%s]", output_pose_topic_name.c_str());
-    publish = node_handle.advertise<leap_controller_capstone::HandPoseStamped>(output_pose_topic_name, 1);
 
     //Heh. Funny.
     //Prevents anything from happening until the user is ready.
@@ -274,8 +158,8 @@ int main(int argc, char** argv) {
     std::cin.get();
 
     // Create a sample listener and controller
-    LeapController listener;
-    Controller controller;
+    LeapController listener = LeapController(node_handle);
+    Leap::Controller controller;
 
     // Have the sample listener receive events from the controller
     controller.addListener(listener);
