12b33d40a08398ce58b731dedad5287d4d77b585
rafle10 <rafle10@users.noreply.github.com>
2018-01-31 19:41:21 -0500

    Removed/commented out unnecessary/arm code/values
    arm code is the values that were published for the arm project. Do not delete for the time being. Hand data points array still untested

--- src/LeaptoROSArm.cpp
+++ src/LeaptoROSArm.cpp
@@ -68,9 +68,9 @@ void SampleListener::onFrame(const Controller& controller) {
     long rollPitchYaw[3];
 
     //Struct of the 3 points that I need to send via ROS MSG
-    geometry_msgs::PoseStamped sensedPoseTip1;//Thumb Finger tip
-    geometry_msgs::PoseStamped sensedPoseTip2;//Index Finger tip
-    geometry_msgs::PoseStamped sensedPosePalm;//wrist
+//     geometry_msgs::PoseStamped sensedPoseTip1;//Thumb Finger tip
+//     geometry_msgs::PoseStamped sensedPoseTip2;//Index Finger tip
+//     geometry_msgs::PoseStamped sensedPosePalm;//wrist
 
     for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
         // Get the first hand
@@ -79,19 +79,14 @@ void SampleListener::onFrame(const Controller& controller) {
         //FIRST HAND ONLY
         if(hand == frame.hands() [0]){
             std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
-            std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
-            << ", palm position: " << hand.palmPosition() << std::endl;
+//             std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
+//             << ", palm position: " << hand.palmPosition() << std::endl;
 
             // Get the hand's normal vector and direction
             const Vector normal = hand.palmNormal();
             const Vector direction = hand.direction();
-
-            // Calculate the hand's pitch, roll, and yaw angles
-            std::cout << std::string(2, ' ') <<  "pitch: " << direction.pitch() << " Rads, "
-            << "roll: " << normal.roll() << " Rads, "
-            << "yaw: " << direction.yaw() << " Rads" << std::endl;
          
-//Adding Roll, Pitch, Yaw
+//Adding Roll, Pitch, Yaw in mm
             rollPitchYaw[0] = normal.roll();
             rollPitchYaw[1] = direction.pitch();
             rollPitchYaw[2] = direction.yaw();
@@ -107,9 +102,9 @@ void SampleListener::onFrame(const Controller& controller) {
                    || fingerNames[finger.type()] == fingerNames[2]){
                     std::cout << std::string(4, ' ') <<  fingerNames[finger.type()]
 //maybe delete info below.
-                    << " finger, id: " << finger.id()
-                    << ", length: " << finger.length()
-                    << "mm, width: " << finger.width() << std::endl;
+//                     << " finger, id: " << finger.id()
+//                     << ", length: " << finger.length()
+//                     << "mm, width: " << finger.width() << std::endl;
                 }
 
                 //The Start and End points of each bone is the frame coordinates of where the bones begin and end
@@ -122,90 +117,61 @@ void SampleListener::onFrame(const Controller& controller) {
                     handDataPoints[DataPointCounter] = bone.prevJoint();
 
                     //Center of Palm = (Middle Metacarpal Start + End)/2 (x,y,z) (Yaw, Roll, Pitch)
-                    if(fingerNames[finger.type()] == fingerNames[3] && boneNames[boneType] == boneNames[0]){
-                            //Center of Palm
-                            std::cout << std::string(6, ' ') <<  "Center of Palm " << bone.center() << std::endl;
-
+//                     if(fingerNames[finger.type()] == fingerNames[3] && boneNames[boneType] == boneNames[0]){
+                            
                             //Copied from Kyle Frizzell's myDummyPub publisher
                             //Values in meters (mm/1000).
-                            sensedPosePalm.header.frame_id = "m1n6s200_link_base";
-                            sensedPosePalm.pose.position.x = -(bone.center().x/1000);
-                            sensedPosePalm.pose.position.y = (bone.center().z/1000);
-                            sensedPosePalm.pose.position.z = (bone.center().y/1000);
-
-                            //Conversion Trial1: For ARM settings
-                            // double yaw = (bone.prevJoint().roll());
-                            // double roll = (bone.prevJoint().pitch());
-                            // double pitch = (bone.prevJoint().yaw());
-
-                            //Finger Based Values
-                            // double yaw = (bone.prevJoint().yaw());
-                            // double roll = (bone.prevJoint().roll());
-                            // double pitch = (bone.prevJoint().pitch());
+//                             sensedPosePalm.header.frame_id = "m1n6s200_link_base";
+//                             sensedPosePalm.pose.position.x = -(bone.center().x/1000);
+//                             sensedPosePalm.pose.position.y = (bone.center().z/1000);
+//                             sensedPosePalm.pose.position.z = (bone.center().y/1000);
 
                             //Hand based values
-                            double yaw = -direction.yaw();
-                            // double roll = normal.roll();
-                            double roll = normal.roll();
-                            double pitch = direction.pitch();
-                            std::cout << "Normal   (R,P,Y): (" << normal.roll() << ", " << normal.pitch() << ", " << normal.yaw() << ")" << std::endl;
-                            std::cout << "Direction (R,P,Y): (" << direction.roll() << ", " << direction.pitch() << ", " << direction.yaw() << ")" << std::endl;
-                            //Hand based values Conversion Trials 1
-                            // double pitch = direction.yaw();
-                            // double yaw = normal.roll();
-                            // double roll = direction.pitch();
-
-                            //Hand based values Conversion Trials 2
-                            // double roll = direction.yaw();
-                            // double yaw = normal.roll();
-                            // double pitch = direction.pitch();
+//                             double yaw = -direction.yaw();
+//                             double roll = normal.roll();
+//                             double pitch = direction.pitch();
 
                             //Vector Converter for ROS
                             //Changes yaw, pitch, roll into
                             //(yaw pitch roll angle);(x y z w)
-                            tf::Matrix3x3 space;
-                            space.setEulerYPR(yaw,pitch,roll);
-                            tf::Quaternion quater;
-                            space.getRotation(quater);
+//                             tf::Matrix3x3 space;
+//                             space.setEulerYPR(yaw,pitch,roll);
+//                             tf::Quaternion quater;
+//                             space.getRotation(quater);
 
-                            tf::Quaternion quater2 = tf::createQuaternionFromRPY(roll, pitch, yaw);
-
-
+//                             tf::Quaternion quater2 = tf::createQuaternionFromRPY(roll, pitch, yaw);
 
                             //Puts converted data into the ROS struct
-                            std::cout << "Quater Values: " << std::endl
-                            << "X: " << quater.getX() << "  Y: " << quater.getY()
-                            << "  Z: " << quater.getZ() << "  W: " << quater.getW() << std::endl;
+//                             std::cout << "Quater Values: " << std::endl
+//                             << "X: " << quater.getX() << "  Y: " << quater.getY()
+//                             << "  Z: " << quater.getZ() << "  W: " << quater.getW() << std::endl;
 
-                            std::cout << "Second Quater Values: " << std::endl
-                            << "X: " << quater2.getX() << "  Y: " << quater2.getY()
-                            << "  Z: " << quater2.getZ() << "  W: " << quater2.getW() << std::endl;
-                            sensedPosePalm.pose.orientation.x = quater.getX();
-                            sensedPosePalm.pose.orientation.y = quater.getY();
-                            sensedPosePalm.pose.orientation.z = quater.getZ();
-                            sensedPosePalm.pose.orientation.w = quater.getW();
+//                             std::cout << "Second Quater Values: " << std::endl
+//                             << "X: " << quater2.getX() << "  Y: " << quater2.getY()
+//                             << "  Z: " << quater2.getZ() << "  W: " << quater2.getW() << std::endl;
+//                             sensedPosePalm.pose.orientation.x = quater.getX();
+//                             sensedPosePalm.pose.orientation.y = quater.getY();
+//                             sensedPosePalm.pose.orientation.z = quater.getZ();
+//                             sensedPosePalm.pose.orientation.w = quater.getW();
                     }
 
                     //Finger Tip Thumb = Thumb Distal Start (x,y,z)
-                    if(fingerNames[finger.type()] == fingerNames[0] && boneNames[boneType] == boneNames[3]){
-                            std::cout << std::string(6, ' ') << "Finger Tip: " << bone.prevJoint() << std::endl;
-                            sensedPoseTip1.header.frame_id = "m1n6s200_link_base";
-                            sensedPoseTip1.pose.position.x = -(double)((bone.prevJoint().x)/1000);
-                            sensedPoseTip1.pose.position.y = (double)((bone.prevJoint().z)/1000);
-                            sensedPoseTip1.pose.position.z = (double)((bone.prevJoint().y)/1000);
-                    }
+//                     if(fingerNames[finger.type()] == fingerNames[0] && boneNames[boneType] == boneNames[3]){
+//                             std::cout << std::string(6, ' ') << "Finger Tip: " << bone.prevJoint() << std::endl;
+//                             sensedPoseTip1.header.frame_id = "m1n6s200_link_base";
+//                             sensedPoseTip1.pose.position.x = -(double)((bone.prevJoint().x)/1000);
+//                             sensedPoseTip1.pose.position.y = (double)((bone.prevJoint().z)/1000);
+//                             sensedPoseTip1.pose.position.z = (double)((bone.prevJoint().y)/1000);
+//                     }
 
                     //Finger Tip Index = Index Distal Start (x,y,z)
                     //Changed to Index proximal (start + end)/2 (x,y,z)
-                    if(fingerNames[finger.type()] == fingerNames[1] && boneNames[boneType] == boneNames[1]){
-                            std::cout << std::string(6, ' ') << "Index \"Finger Tip\": " << bone.prevJoint() << std::endl;
-                            sensedPoseTip2.header.frame_id = "m1n6s200_link_base";
-                            sensedPoseTip2.pose.position.x = -(double)((bone.prevJoint().x)/1000);
-                            sensedPoseTip2.pose.position.y = (double)((bone.prevJoint().z)/1000);
-                            sensedPoseTip2.pose.position.z = (double)((bone.prevJoint().y)/1000);
-                            // sensedPoseTip2.pose.position.x = -(double)((bone.prevJoint().x)/1000);
-                            // sensedPoseTip2.pose.position.y = (double)((bone.prevJoint().z)/1000);
-                            // sensedPoseTip2.pose.position.z = (double)((bone.prevJoint().y)/1000);
+//                     if(fingerNames[finger.type()] == fingerNames[1] && boneNames[boneType] == boneNames[1]){
+//                             std::cout << std::string(6, ' ') << "Index \"Finger Tip\": " << bone.prevJoint() << std::endl;
+//                             sensedPoseTip2.header.frame_id = "m1n6s200_link_base";
+//                             sensedPoseTip2.pose.position.x = -(double)((bone.prevJoint().x)/1000);
+//                             sensedPoseTip2.pose.position.y = (double)((bone.prevJoint().z)/1000);
+//                             sensedPoseTip2.pose.position.z = (double)((bone.prevJoint().y)/1000);
                     }
                 }
              DataPointCounter++;
@@ -214,28 +180,27 @@ void SampleListener::onFrame(const Controller& controller) {
     }
     if(hands.isEmpty()){
       // std::cout << "Error State: No Hands" << std::endl;
-      sensedPoseTip1.header.frame_id = "m1n6s200_link_base";
-      sensedPoseTip1.pose.position.x = outOfBoundsState;
-      sensedPoseTip1.pose.position.y = outOfBoundsState;
-      sensedPoseTip1.pose.position.z = outOfBoundsState;
+//       sensedPoseTip1.header.frame_id = "m1n6s200_link_base";
+//       sensedPoseTip1.pose.position.x = outOfBoundsState;
+//       sensedPoseTip1.pose.position.y = outOfBoundsState;
+//       sensedPoseTip1.pose.position.z = outOfBoundsState;
 
-      sensedPoseTip2.header.frame_id = "m1n6s200_link_base";
-      sensedPoseTip2.pose.position.x = outOfBoundsState;
-      sensedPoseTip2.pose.position.y = outOfBoundsState;
-      sensedPoseTip2.pose.position.z = outOfBoundsState;
+//       sensedPoseTip2.header.frame_id = "m1n6s200_link_base";
+//       sensedPoseTip2.pose.position.x = outOfBoundsState;
+//       sensedPoseTip2.pose.position.y = outOfBoundsState;
+//       sensedPoseTip2.pose.position.z = outOfBoundsState;
 
-      sensedPosePalm.header.frame_id = "m1n6s200_link_base";
-      sensedPosePalm.pose.position.x = outOfBoundsState;
-      sensedPosePalm.pose.position.y = outOfBoundsState;
-      sensedPosePalm.pose.position.z = outOfBoundsState;
-
+//       sensedPosePalm.header.frame_id = "m1n6s200_link_base";
+//       sensedPosePalm.pose.position.x = outOfBoundsState;
+//       sensedPosePalm.pose.position.y = outOfBoundsState;
+//       sensedPosePalm.pose.position.z = outOfBoundsState;
     }
     //Puts everything in one nice package and sends the data
-    leap_controller_capstone::HandPoseStamped thread;
+//     leap_controller_capstone::HandPoseStamped thread;
     //thread.poseTip2 = sensedPoseTip2;
     //thread.poseTip1 = sensedPoseTip1;
-    thread.posePalm = sensedPosePalm;
-    publish.publish(thread);
+//     thread.posePalm = sensedPosePalm;
+//     publish.publish(thread);
 }
 
 void SampleListener::onFocusGained(const Controller& controller) {
@@ -267,14 +232,15 @@ void SampleListener::onServiceDisconnect(const Controller& controller) {
 int main(int argc, char** argv) {
 
     //Defines where the data is to be sent to.
-    ros::init(argc, argv, "leap_controller_node");
-    ros::NodeHandle node_handle("~");
-    std::string output_pose_topic_name;
-    node_handle.getParam("output_pose_topic",output_pose_topic_name);
-    publish = node_handle.advertise<leap_controller_capstone::HandPoseStamped>(output_pose_topic_name, 1);
+//     ros::init(argc, argv, "leap_controller_node");
+//     ros::NodeHandle node_handle("~");
+//     std::string output_pose_topic_name;
+//     node_handle.getParam("output_pose_topic",output_pose_topic_name);
+//     publish = node_handle.advertise<leap_controller_capstone::HandPoseStamped>(output_pose_topic_name, 1);
 
     //Heh. Funny.
     //Prevents anything from happening until the user is ready.
+    //Remove for something else
     std::cout << "Touch Trump's Hair to start (Enter).."<< std::endl;
     std::cin.get();
 
