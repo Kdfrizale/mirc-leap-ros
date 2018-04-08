20711cb05d8a4429e16dd2d34d92e0bc18bb85c2
David Conner <david.conner@cnu.edu>
2018-04-08 00:42:18 -0400

    rework for standard ROS

---
+++ include/leap_controller.h
@@ -0,0 +1,82 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2018
+ * Capable Humanitarian Robotics and Intelligent Systems Lab (CHRISLab)
+ * Christopher Newport University
+ *
+ *  This code is based on sample code from LeapMotionSDK
+ *  Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Christopher Newport University, TU Darmstadt,
+ *     Team ViGIR, nor the names of its contributors may be used to endorse
+ *     or promote products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+#include <Leap.h>
+#include "ros/ros.h"
+#include <leap_controller/HandPoseStamped.h>
+
+class LeapController : public Leap::Listener {
+public:
+    LeapController(ros::NodeHandle &nh);
+    virtual void onInit(const Leap::Controller&);
+    virtual void onConnect(const Leap::Controller&);
+    virtual void onDisconnect(const Leap::Controller&);
+    virtual void onExit(const Leap::Controller&);
+    virtual void onFrame(const Leap::Controller&);
+    virtual void onFocusGained(const Leap::Controller&);
+    virtual void onFocusLost(const Leap::Controller&);
+    virtual void onDeviceChange(const Leap::Controller&);
+    virtual void onServiceConnect(const Leap::Controller&);
+    virtual void onServiceDisconnect(const Leap::Controller&);
+
+    enum HandToSenseEnum {FIRST_HAND, LEFT_HAND, RIGHT_HAND, BOTH_HANDS};
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher hand_pose_publisher_;
+    LeapController::HandToSenseEnum hand_to_sense_;
+    std::vector<Leap::Finger::Type> fingers_to_track_;
+    std::string relative_frame_;
+
+    double xOffset_;
+    double yOffset_;
+    double zOffset_;
+    double rollOffset_;
+    double pitchOffset_;
+    double yawOffset_;
+
+    leap_controller::HandPoseStamped current_hand_msg_;
+    Leap::Frame current_frame_;
+
+    void processFrame();
+    void processHand(const Leap::Hand& aHand);
+    void processFinger(const Leap::Finger& aFinger);
+    void publishHandPose();
+    void resetMessageInfo();
+    LeapController::HandToSenseEnum convertToHandSenseEnum(std::string const& aString);
+    Leap::Finger::Type convertToFingerType(std::string const& aString);
+};
--- src/leap_controller.cpp
+++ src/leap_controller.cpp
@@ -1,58 +1,51 @@
-/*
- Based off of the very elaborate sample provided by the Leap Motion Developement team
- with the necessary adjustments and added code to use for my controller's intents and purposes.
- All rights reserved to the their proper place.
- Author: Ricardo Flores
- */
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2018
+ * Capable Humanitarian Robotics and Intelligent Systems Lab (CHRISLab)
+ * Christopher Newport University
+ *
+ *  This code is based on sample code from LeapMotionSDK
+ *  Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Christopher Newport University, TU Darmstadt,
+ *     Team ViGIR, nor the names of its contributors may be used to endorse
+ *     or promote products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Authors: Ricardo Flores, Kyle Frizzell, and David Conner */
 
 #include <iostream>
 #include <cstring>
-#include "../ignore/Leap.h"
-#include "ros/ros.h"
-#include <leap_controller_capstone/HandPoseStamped.h>
-#include <leap_controller_capstone/FingerPoseStamped.h>
+
+#include <leap_controller/FingerPoseStamped.h>
 #include <geometry_msgs/PoseStamped.h>
-#include "../ignore/tf/LinearMath/Matrix3x3.h"
-#include <boost/shared_ptr.hpp>
-#include "../ignore/tf/transform_datatypes.h"
-
-class LeapController : public Leap::Listener {
-public:
-    LeapController(ros::NodeHandle &nh);
-    virtual void onInit(const Leap::Controller&);
-    virtual void onConnect(const Leap::Controller&);
-    virtual void onDisconnect(const Leap::Controller&);
-    virtual void onExit(const Leap::Controller&);
-    virtual void onFrame(const Leap::Controller&);
-    virtual void onFocusGained(const Leap::Controller&);
-    virtual void onFocusLost(const Leap::Controller&);
-    virtual void onDeviceChange(const Leap::Controller&);
-    virtual void onServiceConnect(const Leap::Controller&);
-    virtual void onServiceDisconnect(const Leap::Controller&);
-
-    enum HandToSenseEnum {FIRST_HAND, LEFT_HAND, RIGHT_HAND, BOTH_HANDS};
-private:
-    ros::NodeHandle nh_;
-    ros::Publisher hand_pose_publisher_;
-    LeapController::HandToSenseEnum hand_to_sense_;
-    std::vector<Leap::Finger::Type> fingers_to_track_;
-    std::string relative_frame_;
-
-    double xOffset_;
-    double yOffset_;
-    double zOffset_;
-
-    leap_controller_capstone::HandPoseStamped current_hand_msg_;
-    Leap::Frame current_frame_;
-
-    void processFrame();
-    void processHand(const Leap::Hand& aHand);
-    void processFinger(const Leap::Finger& aFinger);
-    void publishHandPose();
-    void resetMessageInfo();
-    LeapController::HandToSenseEnum convertToHandSenseEnum(std::string const& aString);
-    Leap::Finger::Type convertToFingerType(std::string const& aString);
-};
+#include "tf/LinearMath/Matrix3x3.h"
+#include "tf/transform_datatypes.h"
 
 LeapController::LeapController(ros::NodeHandle &nh):nh_(nh){
   std::string output_pose_topic_name;
@@ -61,27 +54,32 @@ LeapController::LeapController(ros::NodeHandle &nh):nh_(nh){
   hand_pose_publisher_ = nh_.advertise<leap_controller_capstone::HandPoseStamped>(output_pose_topic_name,10);
 
   std::string hand_to_sense_tmp;
-  nh_.param<std::string>("leap_controller_node/hand_to_sense",hand_to_sense_tmp, "first");
+  nh_.param<std::string>("hand_to_sense",hand_to_sense_tmp, "first");
   hand_to_sense_ = convertToHandSenseEnum(hand_to_sense_tmp);
 
   std::vector<std::string> fingers_to_track_tmp;
-  nh_.getParam("leap_controller_node/fingers_to_track_list",fingers_to_track_tmp);
+  nh_.getParam("fingers_to_track_list",fingers_to_track_tmp);
   for (std::string finger : fingers_to_track_tmp){
     fingers_to_track_.push_back(convertToFingerType(finger));
   }
 
-  nh_.param<std::string>("leap_controller_node/relative_frame",relative_frame_, "/root");
+  nh_.param<std::string>("base_frame",base_frame_, "/root");
 
-  nh_.param<double>("leap_controller_node/x_offset_position",xOffset_, 0.0);
-  nh_.param<double>("leap_controller_node/y_offset_position",yOffset_, 0.0);
-  nh_.param<double>("leap_controller_node/z_offset_position",zOffset_, 0.0);
+  nh_.param<double>("x_offset_position",xOffset_, 0.0);
+  nh_.param<double>("y_offset_position",yOffset_, 0.0);
+  nh_.param<double>("z_offset_position",zOffset_, 0.0);
+  nh_.param<double>("roll_offset_orientation", rollOffset_,  0.0);
+  nh_.param<double>("pitch_offset_orientation",pitchOffset_, 0.0);
+  nh_.param<double>("yaw_offset_orientation",  yawOffset_,   0.0);
+
+
 }
 
 LeapController::HandToSenseEnum LeapController::convertToHandSenseEnum(std::string const& aString){
-  if (aString == "first") return FIRST_HAND;
-  else if (aString == "left") return LEFT_HAND;
+  if      (aString == "first") return FIRST_HAND;
+  else if (aString == "left")  return LEFT_HAND;
   else if (aString == "right") return RIGHT_HAND;
-  else if (aString == "both") return BOTH_HANDS;
+  else if (aString == "both")  return BOTH_HANDS;
   else {
     ROS_WARN("INVALID hand_to_sense value:[%s]. Possible values are [first,left,right,both]",aString.c_str());
     ROS_INFO("Unrecognized hand_to_sense value. Defaulting to 'first'");
@@ -143,11 +141,12 @@ void LeapController::processFrame(){
 
 void LeapController::processHand(const Leap::Hand& aHand){
   current_hand_msg_.name = aHand.isLeft() ? "left" : "right";
-  current_hand_msg_.header.frame_id = relative_frame_;
-  current_hand_msg_.posePalm.header.frame_id = relative_frame_;
+  current_hand_msg_.header.stamp = ros::Time::now();
+  current_hand_msg_.header.frame_id = base_frame_;
+  current_hand_msg_.posePalm.header.frame_id = base_frame_;
   current_hand_msg_.posePalm.pose.position.x = -(aHand.palmPosition().x/1000) + xOffset_;
-  current_hand_msg_.posePalm.pose.position.y = (aHand.palmPosition().z/1000) + yOffset_;
-  current_hand_msg_.posePalm.pose.position.z = (aHand.palmPosition().y/1000) + zOffset_;
+  current_hand_msg_.posePalm.pose.position.y =  (aHand.palmPosition().z/1000) + yOffset_;
+  current_hand_msg_.posePalm.pose.position.z =  (aHand.palmPosition().y/1000) + zOffset_;
 
   current_hand_msg_.posePalm.pose.orientation.x = 0;
   current_hand_msg_.posePalm.pose.orientation.y = 0;
@@ -193,8 +192,8 @@ void LeapController::processFinger(const Leap::Finger& aFinger){
       switch (bone.type()) {
         case Leap::Bone::TYPE_DISTAL:
           finger_msg.poseDistalPhalange.pose.position.x = -(double)((bone.nextJoint().x)/1000) + xOffset_;//Used nextJoint to get the tip
-          finger_msg.poseDistalPhalange.pose.position.y = (double)((bone.nextJoint().z)/1000) + yOffset_;
-          finger_msg.poseDistalPhalange.pose.position.z = (double)((bone.nextJoint().y)/1000) + zOffset_;
+          finger_msg.poseDistalPhalange.pose.position.y =  (double)((bone.nextJoint().z)/1000) + yOffset_;
+          finger_msg.poseDistalPhalange.pose.position.z =  (double)((bone.nextJoint().y)/1000) + zOffset_;
           //TODO fill in the rest for position and orientation
           //translate to ROS Space here
           //how to do correct orientation?
@@ -202,20 +201,20 @@ void LeapController::processFinger(const Leap::Finger& aFinger){
           break;
         case Leap::Bone::TYPE_INTERMEDIATE:
           finger_msg.poseIntermediatePhalange.pose.position.x = -(double)((bone.center().x)/1000) + xOffset_;
-          finger_msg.poseIntermediatePhalange.pose.position.y = (double)((bone.center().z)/1000) + yOffset_;
-          finger_msg.poseIntermediatePhalange.pose.position.z = (double)((bone.center().y)/1000) + zOffset_;
+          finger_msg.poseIntermediatePhalange.pose.position.y =  (double)((bone.center().z)/1000) + yOffset_;
+          finger_msg.poseIntermediatePhalange.pose.position.z =  (double)((bone.center().y)/1000) + zOffset_;
           //TODO fill in
           break;
         case Leap::Bone::TYPE_PROXIMAL:
           finger_msg.poseProximalPhalange.pose.position.x = -(double)((bone.center().x)/1000) + xOffset_;
-          finger_msg.poseProximalPhalange.pose.position.y = (double)((bone.center().z)/1000) + yOffset_;
-          finger_msg.poseProximalPhalange.pose.position.z = (double)((bone.center().y)/1000) + zOffset_;
+          finger_msg.poseProximalPhalange.pose.position.y =  (double)((bone.center().z)/1000) + yOffset_;
+          finger_msg.poseProximalPhalange.pose.position.z =  (double)((bone.center().y)/1000) + zOffset_;
           //TODO fill in
           break;
         case Leap::Bone::TYPE_METACARPAL:
           finger_msg.poseMetacarpal.pose.position.x = -(double)((bone.center().x)/1000) + xOffset_;
-          finger_msg.poseMetacarpal.pose.position.y = (double)((bone.center().z)/1000) + yOffset_;
-          finger_msg.poseMetacarpal.pose.position.z = (double)((bone.center().y)/1000) + zOffset_;
+          finger_msg.poseMetacarpal.pose.position.y =  (double)((bone.center().z)/1000) + yOffset_;
+          finger_msg.poseMetacarpal.pose.position.z =  (double)((bone.center().y)/1000) + zOffset_;
           //TODO fill in
           break;
       }
@@ -279,21 +278,24 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "leap_controller_node");
     ros::NodeHandle node_handle;
 
+    // Create the LeapController instance
     LeapController listener = LeapController(node_handle);
     Leap::Controller controller;
+
     // Have the sample listener receive events from the controller
     controller.addListener(listener);
+
     //Essentially, it runs everything as a background multi-thread.
     if (argc > 1 && strcmp(argv[1], "--bg") == 0)
         controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
 
-
-    bool should_operate = true;
-    while(ros::ok()  && should_operate){
+    // process the data
+    while(ros::ok()){
       ros::spinOnce();
     }
 
     // Remove the sample listener when done
+    ROS_INFO(" Shutting down LeapMotion listener ... ");
     controller.removeListener(listener);
 
     return 0;
